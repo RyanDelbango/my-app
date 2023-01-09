72cd5c63322f1e8fbbc2e7588e837258c536d76a
Ryan Delbango <ryan.delbango@gmail.com>
2023-01-09 16:26:51 -0500

    Add star wars api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+
+
 
 function App() {
+  const [data, setData] = useState('Loading...')
+  const [planetNumber, setPlanetNumber] = useState(1)
+
+
+useEffect(() => {
+  const fetch = async () => {
+    const body = {
+      planet: planetNumber
+    }
+    setData('Loading...')
+    const { data } = await axios.post('http://localhost:3001/', body )
+    setData(data)
+  }
+
+  fetch()
+}, [planetNumber])
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input value={planetNumber} type="number" id="quantity" name="quantity" min="1" max="5" onChange={(e) => {
+          setPlanetNumber(e.target.value)
+        }} />
       </header>
     </div>
   );
